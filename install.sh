7930eb77994f0dc3843f4c1264e7dbc0e80d5b58
gyeongyeol-choi <gyeongyeol_choi@tmax.co.kr>
2021-05-25 10:26:47 +0000

    tfc-operator 설치/삭제 자동화 스크립트 작성

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo chmod +x version.conf
+source version.conf
+
+cd manifest
+
+if [ $REGISTRY != "{REGISTRY}" ]; then
+	sed -i "s/tmaxcloudck\/tfc-operator/${REGISTRY}\/tfc-operator/g" 04_deployment.yaml
+	sed -i "s/tmaxcloudck\/tfc-worker/${REGISTRY}\/tfc-worker/g" 04_deployment.yaml
+fi
+
+
+sed -i 's/{TFC_VERSION}/'${TFC_VERSION}'/g' 04_deployment.yaml
+
+cd ..
+
+kubectl create -f manifest/
